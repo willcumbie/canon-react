d6db0089fb27d6d750e78fd7ced681ebd61f19c4
adampickeral <adam.pickeral@rackspace.com>
2015-06-15 16:54:58 -0400

    describe placement of popover in relation to the target at which it is pointing

--- src/Popover.jsx
+++ src/Popover.jsx
@@ -1,23 +1,27 @@
 var Popover = React.createClass({
 
+  propTypes: {
+    placement: React.PropTypes.oneOf(['right', 'bottom-right', 'left', 'bottom-left'])
+  },
+
   getDefaultProps: function () {
     return {
-      position: 'left-top'
+      placement: 'right'
     };
   },
 
-  _arrowPosition: function () {
+  _arrowPlacement: function () {
     var arrowClasses, arrowPositions;
 
     arrowPositions = {
-      'left-top': 'rs-popover-arrow-left-top',
-      'top-left': 'rs-popover-arrow-top-left',
-      'right-top': 'rs-popover-arrow-right-top',
-      'top-right': 'rs-popover-arrow-top-right'
+      'right': 'rs-popover-arrow-left-top',
+      'bottom-right': 'rs-popover-arrow-top-left',
+      'left': 'rs-popover-arrow-right-top',
+      'bottom-left': 'rs-popover-arrow-top-right'
     };
     arrowClasses = ['rs-popover-arrow'];
 
-    arrowClasses.push(arrowPositions[this.props.position]);
+    arrowClasses.push(arrowPositions[this.props.placement]);
 
     return arrowClasses.join(' ');
   },
@@ -25,7 +29,7 @@ var Popover = React.createClass({
   render: function () {
     return (
       <div className="rs-popover">
-        <div className={this._arrowPosition()}></div>
+        <div className={this._arrowPlacement()}></div>
         <div className="rs-popover-content">
             {this.props.children}
         </div>
--- test/PopoverSpec.jsx
+++ test/PopoverSpec.jsx
@@ -4,16 +4,16 @@ var TestUtils = React.addons.TestUtils;
 describe('Popover', function () {
   var popover;
 
-  function renderPopover(position) {
+  function renderPopover(placement) {
     popover = TestUtils.renderIntoDocument(
-      <Popover position={position}>
+      <Popover placement={placement}>
         Hello
       </Popover>
     );
   }
 
   it('renders a popover', function () {
-    renderPopover('left-top');
+    renderPopover('right');
 
     expect(popover.getDOMNode()).toHaveClass('rs-popover');
   });
@@ -21,38 +21,38 @@ describe('Popover', function () {
   it('renders children', function () {
     var popoverContent;
 
-    renderPopover('left-top');
+    renderPopover('right');
     popoverContent = TestUtils.findRenderedDOMComponentWithClass(popover, 'rs-popover-content');
 
     expect(popoverContent.getDOMNode().textContent).toBe('Hello');
   });
 
-  describe('arrow position', function () {
+  describe('arrow placement', function () {
 
     function arrow() {
       return TestUtils.findRenderedDOMComponentWithClass(popover, 'rs-popover-arrow').getDOMNode();
     }
 
-    it('left top', function () {
-      renderPopover('left-top');
+    it('right', function () {
+      renderPopover('right');
 
       expect(arrow()).toHaveClass('rs-popover-arrow-left-top');
     });
 
-    it('top left', function () {
-      renderPopover('top-left');
+    it('bottom right', function () {
+      renderPopover('bottom-right');
 
       expect(arrow()).toHaveClass('rs-popover-arrow-top-left');
     });
 
-    it('right top', function () {
-      renderPopover('right-top');
+    it('left', function () {
+      renderPopover('left');
 
       expect(arrow()).toHaveClass('rs-popover-arrow-right-top');
     });
 
-    it('top right', function () {
-      renderPopover('top-right');
+    it('bottom left', function () {
+      renderPopover('bottom-left');
 
       expect(arrow()).toHaveClass('rs-popover-arrow-top-right');
     });
