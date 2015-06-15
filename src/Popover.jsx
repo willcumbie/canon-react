4ae1a899dd23b904866de6a19b576f8a912694dc
adampickeral <adam.pickeral@rackspace.com>
2015-06-15 16:48:54 -0400

    display popover arrow

--- src/Popover.jsx
+++ src/Popover.jsx
@@ -1,20 +1,33 @@
 var Popover = React.createClass({
 
+  getDefaultProps: function () {
+    return {
+      position: 'left-top'
+    };
+  },
+
+  _arrowPosition: function () {
+    var arrowClasses, arrowPositions;
+
+    arrowPositions = {
+      'left-top': 'rs-popover-arrow-left-top',
+      'top-left': 'rs-popover-arrow-top-left',
+      'right-top': 'rs-popover-arrow-right-top',
+      'top-right': 'rs-popover-arrow-top-right'
+    };
+    arrowClasses = ['rs-popover-arrow'];
+
+    arrowClasses.push(arrowPositions[this.props.position]);
+
+    return arrowClasses.join(' ');
+  },
+
   render: function () {
     return (
       <div className="rs-popover">
-        // <div className="rs-popover-arrow rs-popover-arrow-left-top"></div>
-        // <div className="rs-popover-arrow rs-popover-arrow-top-left"></div>
-        // <div className="rs-popover-arrow rs-popover-arrow-top-right"></div>
-        // <div className="rs-popover-arrow rs-popover-arrow-right-top"></div>
+        <div className={this._arrowPosition()}></div>
         <div className="rs-popover-content">
-            <div className="rs-popover-body">
-                {this.props.children}
-            </div>
-            // <div className="rs-popover-footer rs-btn-group">
-            //     <div className="rs-btn rs-btn-primary">Save</div>
-            //     <div className="rs-btn rs-btn-link">Cancel</div>
-            // </div>
+            {this.props.children}
         </div>
       </div>
     );
@@ -22,3 +35,13 @@ var Popover = React.createClass({
 });
 
 module.exports = Popover;
+
+// <div className="rs-popover-arrow rs-popover-arrow-left-top"></div>
+// <div className="rs-popover-arrow rs-popover-arrow-top-left"></div>
+// <div className="rs-popover-arrow rs-popover-arrow-top-right"></div>
+// <div className="rs-popover-arrow rs-popover-arrow-right-top"></div>
+
+// <div className="rs-popover-footer rs-btn-group">
+//     <div className="rs-btn rs-btn-primary">Save</div>
+//     <div className="rs-btn rs-btn-link">Cancel</div>
+// </div>
