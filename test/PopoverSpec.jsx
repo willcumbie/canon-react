792ca4b76a4147496df5e71f405c96eba537852f
adampickeral <adam.pickeral@rackspace.com>
2015-06-15 16:48:54 -0400

    display popover arrow

--- test/PopoverSpec.jsx
+++ test/PopoverSpec.jsx
@@ -4,23 +4,57 @@ var TestUtils = React.addons.TestUtils;
 describe('Popover', function () {
   var popover;
 
-  beforeEach(function () {
+  function renderPopover(position) {
     popover = TestUtils.renderIntoDocument(
-      <Popover>
+      <Popover position={position}>
         Hello
       </Popover>
     );
-  });
+  }
 
   it('renders a popover', function () {
+    renderPopover('left-top');
+
     expect(popover.getDOMNode()).toHaveClass('rs-popover');
   });
 
   it('renders children', function () {
-    var popoverBody;
+    var popoverContent;
 
-    popoverBody = TestUtils.findRenderedDOMComponentWithClass(popover, 'rs-popover-body');
+    renderPopover('left-top');
+    popoverContent = TestUtils.findRenderedDOMComponentWithClass(popover, 'rs-popover-content');
 
-    expect(popoverBody.getDOMNode().textContent).toBe('Hello');
+    expect(popoverContent.getDOMNode().textContent).toBe('Hello');
+  });
+
+  describe('arrow position', function () {
+
+    function arrow() {
+      return TestUtils.findRenderedDOMComponentWithClass(popover, 'rs-popover-arrow').getDOMNode();
+    }
+
+    it('left top', function () {
+      renderPopover('left-top');
+
+      expect(arrow()).toHaveClass('rs-popover-arrow-left-top');
+    });
+
+    it('top left', function () {
+      renderPopover('top-left');
+
+      expect(arrow()).toHaveClass('rs-popover-arrow-top-left');
+    });
+
+    it('right top', function () {
+      renderPopover('right-top');
+
+      expect(arrow()).toHaveClass('rs-popover-arrow-right-top');
+    });
+
+    it('top right', function () {
+      renderPopover('top-right');
+
+      expect(arrow()).toHaveClass('rs-popover-arrow-top-right');
+    });
   });
 });
