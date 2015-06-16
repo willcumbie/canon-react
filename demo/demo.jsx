4874c321f93579aa2e5200ae190557964389085e
Edward Mcenrue <edward.mcenrue@gmail.com>
2015-06-16 13:38:04 -0400

    added testing, implementation, and demo for status indicators

--- demo/demo.jsx
+++ demo/demo.jsx
@@ -189,6 +189,59 @@ var ProcessingIndicator = CanonReact.ProgressBar;
           <ProgressBar progress={25} size='xlarge'/>
         </div>
       </div>
+      <div className='rs-detail-section'>
+        <div className='rs-detail-section-header'>
+          <h2>Status Indicators</h2>
+        </div>
+        <div className='rs-detail-section-body'>
+          <table>
+            <thead>
+              <tr>
+                <td>Status Indicator</td>
+                <td>Type</td>
+                <td>Source</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <StatusIndicator status='ok'>Active</StatusIndicator>
+                </td>
+                <td>Ok</td>
+                <td><pre><code>{"<StatusIndicator status='ok'>Active</StatusIndicator>"}</code></pre></td>
+              </tr>
+              <tr>
+                <td>
+                  <StatusIndicator status='processing'>Building</StatusIndicator>
+                </td>
+                <td>Processing</td>
+                <td><pre><code>{"<StatusIndicator status='processing'>Building</StatusIndicator>"}</code></pre></td>
+              </tr>
+              <tr>
+                <td>
+                  <StatusIndicator status='warning'>Warning</StatusIndicator>
+                </td>
+                <td>Warning</td>
+                <td><pre><code>{"<StatusIndicator status='warning'>Warning</StatusIndicator>"}</code></pre></td>
+              </tr>
+              <tr>
+                <td>
+                  <StatusIndicator status='error'>Error</StatusIndicator>
+                </td>
+                <td>Error</td>
+                <td><pre><code>{"<StatusIndicator status='error'>Warning</StatusIndicator>"}</code></pre></td>
+              </tr>
+              <tr>
+                <td>
+                  <StatusIndicator status='disabled'>Disabled</StatusIndicator>
+                </td>
+                <td>Disabled</td>
+                <td><pre><code>{"<StatusIndicator status='disabled'>Disabled</StatusIndicator>"}</code></pre></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>,
     document.getElementById('content')
   );
---
+++ src/StatusIndicator.jsx
@@ -0,0 +1,45 @@
+var StatusIndicator = React.createClass({
+  propTypes: {
+    status: React.PropTypes.string,
+    hidden: React.PropTypes.bool
+  },
+  getDefaultProps: function (){
+    return {
+      status: "ok",
+      hidden: false
+    };
+  },
+  render: function () {
+    return (
+      <statusindicator {...this.props} className={this._classes()}>
+        {this.props.children}
+      </statusindicator>
+    );
+  },
+  _classes: function () {
+    var classes, statusIndicatorTypes;
+
+    statusIndicatorTypes = {
+      "error": "rs-status rs-status-error",
+      "processing": "rs-status rs-status-processing",
+      "warning": "rs-status rs-status-warning",
+      "ok": "rs-status rs-status-ok",
+      "disabled": "rs-status rs-status-disabled"
+    };
+
+    classes = [];
+    classes.push(this.props.className);
+
+    if(this.props.status && statusIndicatorTypes[this.props.status]){
+      classes.push(statusIndicatorTypes[this.props.status]);
+    }
+
+    if(this.props.hidden){
+      classes.push('rs-hidden');
+    }
+
+    return classes.join(' ');
+  }
+});
+
+module.exports = StatusIndicator;
