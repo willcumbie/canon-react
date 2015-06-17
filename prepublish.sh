a102a8d7f07914f0d633e1834ec6b9df554129e1
SethNute <sethnute@gmail.com>
2015-06-17 16:28:15 -0400

    [NPM Module] Still need to change build config to require react as a global var or not

--- prepublish.sh
+++ prepublish.sh
@@ -2,12 +2,10 @@
 
 set -o errexit
 
-git stash save 'Before publish'
 rm -rf dist
 mkdir dist
 npm run build
 mv canon-react.js ./dist
 mv canon-react.min.js ./dist
-rm -rf test-built
-rm test_bundle.js
-git stash pop
+rm -rf test-built ./dist
+rm test_bundle.js ./dist
