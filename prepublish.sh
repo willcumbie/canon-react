5d4fd1fc01f19c1f5de0a9171bf18307c9e5179f
SethNute <sethnute@gmail.com>
2015-06-17 16:47:24 -0400

    Should fix failing prepublish on travis for real

--- prepublish.sh
+++ prepublish.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-set -o errexit
-
-rm -rf dist || true
+mkdir -p dist
+rm -rf dist
 mkdir dist
 npm run build
 mv canon-react.js ./dist
 mv canon-react.min.js ./dist
-rm -rf test-built ./dist
-rm test_bundle.js ./dist
+rm -rf test-built
+rm test_bundle.js
