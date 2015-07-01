791f3f233be9775bdf550ec8812c85ce9b5a8500
adampickeral <adam.pickeral@rackspace.com>
2015-07-01 16:19:52 -0400

    Add demo of popover. add popover components to library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 module.exports = {
   Button: require('./Button'),
   ButtonGroup: require('./ButtonGroup'),
+  Popover: require('./Popover'),
+  PopoverBody: require('./PopoverBody'),
+  PopoverFooter: require('./PopoverFooter'),
+  PopoverTrigger: require('./PopoverTrigger'),
   ProcessingIndicator: require('./ProcessingIndicator'),
   ProgressBar: require('./ProgressBar')
 };
