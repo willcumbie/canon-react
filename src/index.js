b52a551195df5f5195ca1b6fb28eb9d84b9452bb
Joshua Rush <jdrush89@gmail.com>
2015-07-06 16:19:16 -0400

    Adding Dropdown components to exports index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 module.exports = {
   Button: require('./Button'),
   ButtonGroup: require('./ButtonGroup'),
+  Divider: require('./Divider'),
+  Dropdown: require('./Dropdown'),
+  DropdownItem: require('./DropdownItem'),
+  DropdownTrigger: require('./DropdownTrigger'),
   Popover: require('./Popover'),
   PopoverBody: require('./PopoverBody'),
   PopoverFooter: require('./PopoverFooter'),
